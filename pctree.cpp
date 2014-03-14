97d6fef14f6a026ac0ef1a0c758b0a7b04a50e23
mguzhou <mguzhou@gmail.com>
2014-03-13 17:51:29 -0700

    Started initialization function

--- pctree.cpp
+++ pctree.cpp
@@ -1,5 +1,18 @@
 #include "pctree.h"
 
-PCtree::PCtree(int** m) {
+void PCtree::PCtree(int** m) {
+
+}
+
+void PCtree:intializeTree(int** m) {
+	Pnode parent;
+	PCarc *prev;
+	PCarc *initial;
+	int i;
+
+	for(i = 0;i<numLeaves;i++){
+		PCarc a,b;
+	}
+		  
 
 }
